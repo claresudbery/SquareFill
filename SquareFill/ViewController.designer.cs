ff38a2021ce0a8916909dbe4b9ae361b970580e0
Clare Sudbery <claresudbery@Clares-MacBook-Pro.local>
2017-02-19 15:00:18 +0000

    Removed UI elements from iOS tutorial.

--- SquareFill/ViewController.designer.cs
+++ SquareFill/ViewController.designer.cs
@@ -13,34 +13,8 @@ namespace SquareFill
     [Register ("ViewController")]
     partial class ViewController
     {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton CallButton { get; set; }
-
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextField PhoneNumberText { get; set; }
-
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton TranslateButton { get; set; }
-
         void ReleaseDesignerOutlets ()
         {
-            if (CallButton != null) {
-                CallButton.Dispose ();
-                CallButton = null;
-            }
-
-            if (PhoneNumberText != null) {
-                PhoneNumberText.Dispose ();
-                PhoneNumberText = null;
-            }
-
-            if (TranslateButton != null) {
-                TranslateButton.Dispose ();
-                TranslateButton = null;
-            }
         }
     }
 }
